d6142c795ecb16b4246c5261a562d0f9832c81eb
周捷 <zhoujie@meiriq.com>
2015-09-10 17:52:57 +0800

    添加 promise yield测试

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+
+/// <reference path="typings/tsd.d.ts" />
+
 var koa = require('koa');
 var logger = require('koa-logger');
 var koaBody = require('koa-body');
@@ -7,12 +10,14 @@ var views = require("koa-views");
 var router = require('koa-router')();
 var path = require('path');
 var app = koa();
-
-
-
+var request=require("request");
+var fs = require("fs");
+var co = require("co");
 // logger
 app.use(logger());
-
+var Promise = require('bluebird');
+Promise.promisifyAll(fs);
+Promise.promisifyAll(request)
 // Must be used before any router is used
 app.use(views({
 	root: __dirname + "/views",
@@ -46,9 +51,13 @@ app.use(compress({
 router.get('/', function * (next) {
 	// console.log(views)
 	var text = "HELLO WORLD!!!";
-	yield this.render("basic", {
-		text: text
-	});
+	text = yield fs.readFileAsync("./app.js");
+	text = yield request.getAsync("http://www.baidu.com").get(1)
+	
+	// yield this.render("basic", {
+	// 	text: text
+	// });
+	this.body = text
 })
 
 router.post('/', function * (next) {
@@ -59,4 +68,7 @@ router.post('/', function * (next) {
 
 app.use(router.routes());
 
-app.listen(3000);
+app.listen(4000,function () {
+	console.log("server listening in 4000");
+});
+
